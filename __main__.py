e0e0b07d12b46afc8546ea515124ad86b3824e4f
Ubuntu <azureuser@masterops.3m2ptt0y4fgulgb54epd13c2lb.ax.internal.cloudapp.net>
2021-10-26 11:29:16 +0000

    gpu machine automation

---
+++ __main__.py
@@ -0,0 +1,154 @@
+# This is the masterops machine on Azure
+
+import base64
+from pulumi import Config, Output, export
+import pulumi_azure_native as azure_native
+import pulumi_azure_native.compute as compute
+import pulumi_azure_native.network as network
+import pulumi_azure_native.resources as resources
+
+#config = Config()
+#username = config.require("username")
+#password = config.require("password")
+
+sub="d818cbed-274b-4240-9872-8761fe9e488c"
+storageAccountName="riseaicenter9576892428"
+resource_group_name_storage="rise-ai-center"
+
+# Create a new resource group for this VM
+resource_group_name_base="rise-ai-center"
+resource_group_location="westeurope"
+resource_group = resources.ResourceGroup(resource_group_name_base, location=resource_group_location)
+
+net = network.VirtualNetwork(
+    "gpu-network",
+    resource_group_name=resource_group.name,
+    address_space=network.AddressSpaceArgs(
+        address_prefixes=["10.0.0.0/16"],
+    ),
+    subnets=[network.SubnetArgs(
+        name="default",
+        address_prefix="10.0.1.0/24",
+    )])
+
+public_ip = network.PublicIPAddress(
+    "gpu-ip",
+    resource_group_name=resource_group.name,
+    public_ip_allocation_method=network.IPAllocationMethod.DYNAMIC)
+
+network_iface = network.NetworkInterface(
+    "gpu-nic",
+    resource_group_name=resource_group.name,
+    ip_configurations=[network.NetworkInterfaceIPConfigurationArgs(
+        name="webserveripcfg",
+        subnet=network.SubnetArgs(id=net.subnets[0].id),
+        private_ip_allocation_method=network.IPAllocationMethod.DYNAMIC,
+        public_ip_address=network.PublicIPAddressArgs(id=public_ip.id),
+    )])
+
+# publisher="Canonical"
+# offer="UbuntuServer"
+# sku="18.04-LTS"
+# version="latest"
+
+# disk = compute.Disk("gpuOSDisk",
+#     creation_data=azure_native.compute.CreationDataArgs(
+#         create_option="FromImage",
+#         image_reference=azure_native.compute.ImageDiskReferenceArgs(
+#             id="/Subscriptions/"+sub+"/Providers/Microsoft.Compute/Locations/westeurope/Publishers/"+publisher+"/ArtifactTypes/VMImage/Offers/"+offer+"/Skus/"+sku+"/Versions/latest"
+#         ),
+#     ),
+#     disk_name="gpuOSDisk",
+#     location="westeurope",
+#     os_type="Linux",
+#     hyper_v_generation="V2",
+#     resource_group_name=resource_group.name
+#     )
+
+init_script = """
+#!/bin/bash
+touch test.txt
+
+"""
+# need to add identity part to trigger creation of MSI
+vm = compute.VirtualMachine(
+    "gpu",
+    identity=compute.VirtualMachineIdentityArgs(type="SystemAssigned") ,
+    resource_group_name=resource_group.name,
+    network_profile=compute.NetworkProfileArgs(
+        network_interfaces=[
+            compute.NetworkInterfaceReferenceArgs(id=network_iface.id),
+        ],
+    ),
+    hardware_profile=compute.HardwareProfileArgs(
+        vm_size="Standard_ND40rs_v2",
+    ),
+    os_profile=compute.OSProfileArgs(
+        admin_username="azureuser",
+        computer_name="gpu",
+        custom_data=base64.b64encode(init_script.encode("ascii")).decode("ascii"),
+        linux_configuration=azure_native.compute.LinuxConfigurationArgs(
+            disable_password_authentication=True,
+            ssh=azure_native.compute.SshConfigurationArgs(
+                public_keys=[
+                    azure_native.compute.SshPublicKeyArgs(
+                        key_data="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDBCu0s5YNk4iA/ONZP5CS549sz73VixHbJI2hCj2bcm3M8soYQJGfmHArSYWTPuf1vF24ltcF1n7ZUua4VLA9iajX5GZDkrl4MAeHTSSWtRBNPG2Ss/TqqMNnNjJswLELg40WqU/xB710VWy2Fov1vG9wOx9+iMd+ZlDq8W3Pk3InpTkWhysu3Inech/Y3kLFvJJNlNoSn7exLw53xTzOOAs5XIXMMCbQtG0dBmalU/arKLaPwHrg6klMcZodM2NtotOR6JcLtVIAnzJHwG5EFFX/s05/OWCLd+lFNagrSgQ/JlPdDW8nm/KS8gFGruaXGS2NpSgcbU2UibiH+l1VlTkpT3NRcTVNFq8fLfKt0k5wMOE5iAghRsQu0Nce1SEDcNubzg0VywkSlqxJJBqhibXu8FN6nZaF54DtOsCsHYIPROaq93jJ3OjKx77pWEb7aSXaexN0BhXjzMjEnTDtVw3ShMe5EZ4loiMEbpjbJRfMNvRn9bpSnx2X8hea+q+SNWoSD/nrjVm7W5eYw8LG/mOmFxWRxXPbR/vjqK+u7GyTpH2Z+cRLTi3UUmnzc8G6gal93ezw+b4VgxoWfgU8ncttsEOdI18bpzptcaswNr7HBeg/mURzuTq3kUDozKmoXEsIe/lMf3V64DdFde172AeBJSr5nOpeinRDXIRraIQ==",
+                        path="/home/azureuser/.ssh/authorized_keys"),                    
+                    azure_native.compute.SshPublicKeyArgs(
+                        key_data="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDjhWR1/QifylSFVt+Bm5cK/nLi62dR9HhmNGj+hbvmZwxM5+aZV2QPtUgPj/CVURL6HGxzSsdMDt8jzbRxPkEQCIL5btCAseZ9yqwdK+zfTfNBy/lyZGicifL8iROB2gpx9PUWufFpLWcCKQITSje7Esj1OLE9tat8ncEjrkf2ILJxbs5IXRUHNNQPoEpxty4pw9Lm8KwuR/v2FyWvQyUr8q/luM3HesRBZs+OlYBluylGenttyk+lI9ELseJpzUd5Ccc7Ks5J3Tdlc/DXAt5nblDvHpQO1469q/BdAmbrUKVrachgwzq2blRMCiX1LJiVqrKqFlT1TsrVo1hSoDQN",
+                        path="/home/azureuser/.ssh/authorized_keys")
+                ],
+            ),
+        ),
+    ),
+    storage_profile=compute.StorageProfileArgs(
+        os_disk=compute.OSDiskArgs(
+            create_option=compute.DiskCreateOptionTypes.FROM_IMAGE,
+            name="gpuOSDisk",
+        ),
+        image_reference=compute.ImageReferenceArgs(
+            publisher="canonical",
+            offer="UbuntuServer",
+            sku="18_04-lts-gen2",
+            version="latest",
+        ),
+    )
+)
+
+    # storage_profile=compute.StorageProfileArgs(
+    #     os_disk=compute.OSDiskArgs(
+    #         create_option=compute.DiskCreateOptionTypes.FROM_IMAGE,
+    #         name="masteropsOSDisk",
+    #     ),
+    #     image_reference=compute.ImageReferenceArgs(
+    #         publisher="canonical",
+    #         offer="UbuntuServer",
+    #         sku="18.04-LTS",
+    #         version="latest",
+    #     ),
+    # )
+
+# Assign Key Operations rights to the VM for the Storage Account 
+
+# Pulumi has special constructs for Input and Output to handle asynchronous behavior
+# so if i want to access the result of the resource_group creation above and use it in a role assignment scope, i need to do this:
+#scope = Output.concat("subscriptions/", sub, "/resourceGroups/", resource_group.name, "/providers/Microsoft.Storage/storageAccounts/",storageAccountName)
+scope="subscriptions/"+sub+"/resourceGroups/"+resource_group_name_storage+"/providers/Microsoft.Storage/storageAccounts/"+storageAccountName
+print(scope)
+role_definition_id="/subscriptions/"+sub+"/providers/Microsoft.Authorization/roleDefinitions/81a9662b-bebf-436f-a333-f67b29880f12"
+
+role_assignment = azure_native.authorization.RoleAssignment("roleAssignment",
+    principal_id=vm.identity.principal_id,
+    principal_type="ServicePrincipal",
+#    role_assignment_name="05c5a614-a7d6-4502-b150-c2fb455033ff",
+    role_definition_id=role_definition_id,
+    scope=scope)
+
+# what is this, a list
+combined_output = Output.all(vm.id, public_ip.name, resource_group.name)
+public_ip_addr = combined_output.apply(
+    lambda lst: network.get_public_ip_address(
+        public_ip_address_name=lst[1], 
+        resource_group_name=lst[2]))
+export("public_ip", public_ip_addr.ip_address)
+export("principal_id", vm.identity.principal_id)
---
+++ mount_fileshare.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# mounts an Azure File Share to a VM
+# uses the json parser jq
+# assumes that this VM has the right role assigned
+
+sudo apt -qq update
+sudo apt -qq install -y jq
+
+sub="d818cbed-274b-4240-9872-8761fe9e488c"
+resourceGroupName="rise-ai-center"
+storageAccountName="riseaicenter9576892428"
+fileShareName="azureml-filestore-a3577153-b708-4da7-ba96-d3d8997a0cac"
+
+# get the Azure storage account key
+# assumes that this VM has the right role assigned
+accessToken=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' -H Metadata:true | jq -r .access_token)
+storageAccountKey=$(curl -s https://management.azure.com/subscriptions/$sub/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$storageAccountName/listKeys?api-version=2016-12-01 --request POST -d "" -H "Authorization: Bearer $accessToken" | jq -r .keys[0].value)
+
+fileStoreEndpoint=https://riseaicenter9576892428.file.core.windows.net/
+
+# diagnostics
+# httpEndpoint=$fileStoreEndpoint
+# smbPath=$(echo $httpEndpoint | cut -c7-$(expr length $httpEndpoint))
+# fileHost=$(echo $smbPath | tr -d "/")
+# nc -zvw3 $fileHost 445
+
+smbPath=$(echo $httpEndpoint | cut -c7-$(expr length $httpEndpoint))$fileShareName
+
+mntRoot="/mount"
+mntPath="$mntRoot/$storageAccountName/$fileShareName"
+
+# echo $smbPath $mntPath $fileHost
+
+sudo mkdir -p $mntPath
+
+sudo mount -t cifs $smbPath $mntPath -o username=$storageAccountName,password=$storageAccountKey,serverino,gid=1000,uid=1000
+ln -s $mntPath /home/azureuser/persistent-files
